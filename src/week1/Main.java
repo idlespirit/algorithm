8de3a1291d942285161b34434ef663654ca722db
Iluska <destroymol@gmail.com>
2015-12-16 21:22:53 +0300

    test

--- src/week1/Main.java
+++ src/week1/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //this is a test
         Scanner scanner = new Scanner(System.in);
         int length = scanner.nextInt();
         int [] arr1 = new int[length+1];
